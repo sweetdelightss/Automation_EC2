b326f672d2d0ccded926ebdfdbdd89959b73862e
Stacey Lokey <52264235+sweetdelightss@users.noreply.github.com>
2021-06-22 21:19:42 -0400

    Add files via upload

---
+++ aws_ec2_create_instance.py
@@ -0,0 +1,41 @@
+# Create EC2 instance using Python BOTO3 with two volumes and two users
+import boto3
+
+
+def create_ec2_instance():
+    try:
+        print("Creating EC2 instance")
+        resource_ec2 = boto3.client("ec2")
+        resource_ec2.run_instances(
+            ImageId="ami-0d8d212151031f51c",
+            MinCount=1,
+            MaxCount=1,
+            InstanceType="t2.micro",
+            KeyName="ec2-key"
+        )
+    except Exception as e:
+        print(e)
+
+
+create_ec2_instance()
+
+# create IAM client
+iam = boto3.client('iam')
+
+# create user
+response = iam.create_user(
+    UserName='user1'
+)
+print(response)
+
+# create user
+response = iam.create_user(
+    UserName='user2'
+)
+print(response)
+
+# List users with pagination interface
+paginator = iam.get_paginator('list_users')
+for response in paginator.paginate():
+    print(response)
+
